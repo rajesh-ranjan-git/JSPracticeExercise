1300e578108d035f6b6b0868f3765759fb390220
Rajesh <rajeshranjan8271@gmail.com>
2025-03-05 06:49:12 +0530

    new problems

---
+++ newProblems3.js
@@ -0,0 +1,27 @@
+// 2. Find the missing number from array
+
+const arr1 = [1, 2, 5, 9];
+const arr2 = [3, 4, 5, 6, 9];
+const arr3 = [];
+let i = 0;
+let j = 0;
+
+console.log(i, j);
+
+while (i < arr1.length && j < arr2.length) {
+  if (arr1[i] < arr2[j]) {
+    arr3.push(arr1[i++]);
+  } else {
+    arr3.push(arr2[j++]);
+  }
+}
+
+while (i < arr1.length) {
+  arr3.push(arr1[i++]);
+}
+
+while (j < arr2.length) {
+  arr3.push(arr2[j++]);
+}
+
+console.log(arr3);
