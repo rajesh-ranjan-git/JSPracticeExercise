45d7f8638c4d56345ab7821e3a76b02c674e24f3
Rajesh <rajeshranjan8271@gmail.com>
2025-03-12 07:00:24 +0530

    Practicing

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-const str = "javascript";
+const arr13 = [0, null, undefined, NaN, 1, "hello"];
+
+console.log(
+  arr13.filter(
+    (item) => item !== null && item !== undefined && !Number.isNaN(item)
+  )
+);
